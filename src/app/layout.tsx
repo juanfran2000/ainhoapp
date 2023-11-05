e0f702ea82adaead7cb3a0092d3ff15145a574a7
jortegaruiz <jortegaruiz@gmail.com>
2023-11-05 12:19:05 -0500

    botones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,12 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <header>
           <Nav />
         </header>
-        <main className="h-screen">{children}</main>
-        <footer className="mt-auto">
+        <main>{children}</main>
+        <footer>
           <Footer />
         </footer>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,84 @@
+"use client";
+import Boton from "@/components/Boton";
+import { useState } from "react";
 export default function Home() {
-  return <div>Hola Ainhoa</div>;
+  const [audio] = useState(new Audio("/plim.mp3"));
+
+  const sonidoPlim = () => {
+    audio.play();
+  };
+  return (
+    <div className=" flex items-center justify-center mb-10">
+      <div className="flex flex-col items-center justify-center py-10 bg-zinc-200 w-11/12 rounded-xl">
+        <h3
+          className="text-2xl bg-white py-2 px-6 rounded-full text-blue-500 transition-all hover:scale-110"
+          onClick={sonidoPlim}
+        >
+          Presiona & Escucha
+        </h3>
+        <div className="px-10 mt-10 flex flex-wrap gap-x-4 gap-y-4 justify-center">
+          <Boton
+            img="/cereal.png"
+            alt="foto cereal"
+            color="bg-green-500"
+            audioSrc="/cereal.mp3"
+          />
+          <Boton
+            img="/galletas.png"
+            alt="foto galletas"
+            color="bg-pink-500"
+            audioSrc="/galletas.mp3"
+          />
+          <Boton
+            img="/alfajor.png"
+            alt="foto alfajor"
+            color="bg-blue-500"
+            audioSrc="/alfajor.mp3"
+          />
+          <Boton
+            img="/galletas-fresa.png"
+            alt="foto galletas fresa"
+            color="bg-red-500"
+            audioSrc="/fresa.mp3"
+          />
+          <Boton
+            img="/monopatin.png"
+            alt="foto monopatin"
+            color="bg-pink-300"
+            audioSrc="/motopatin.mp3"
+          />
+          <Boton
+            img="/jugo.png"
+            alt="foto jugo"
+            color="bg-yellow-300"
+            audioSrc="/jugo.mp3"
+          />
+          <Boton
+            img="/leche.png"
+            alt="foto leche"
+            color="bg-orange-500"
+            audioSrc="/almendra.mp3"
+          />
+          <Boton
+            img="/queso.png"
+            alt="foto queso"
+            color="bg-purple-400"
+            audioSrc="/kiros.mp3"
+          />
+          <Boton
+            img="/yogurt.png"
+            alt="foto yogurt"
+            color="bg-red-300"
+            audioSrc="/yogurt.mp3"
+          />
+          <Boton
+            img="/quinoa.png"
+            alt="foto quinoa"
+            color="bg-yellow-400"
+            audioSrc="/quinoa.mp3"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
